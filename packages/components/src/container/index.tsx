d5d891ff08b4f949d9eeff2fdb05202a62cfb818
Emiya·Leon <wangs.coder@gmail.com>
2024-04-28 11:27:36 +0800

    feat(component): Modify the container

--- packages/components/src/container/index.tsx
+++ packages/components/src/container/index.tsx
@@ -1,5 +1,6 @@
 import type { HTMLAttributes, ReactNode } from 'react';
 import clsx from 'clsx';
+import './container.less';
 
 export interface IContainerProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
@@ -11,19 +12,15 @@ export default function Container({
   children,
   full = false,
   shadow = false,
+  className,
   ...props
 }: IContainerProps) {
   return (
-    <div
-      className={`tw-overflow-hidden tw-flex-auto tw-h-full tw-min-h-0${
-        !full ? ' tw-p-5' : ''
-      }`}
-    >
+    <div className={clsx('emiya-container', className, { full })}>
       <div
-        className={clsx(
-          'tw-flex tw-flex-col tw-overflow-hidden tw-h-full tw-relative tw-bg-white tw-p-5',
-          { 'tw-rounded-md tw-shadow-[0_0_6px_#a1a0a0]': !full && shadow },
-        )}
+        className={clsx('emiya-container__content', {
+          'tw-rounded-md tw-shadow-[0_0_6px_#a1a0a0]': !full && shadow,
+        })}
         {...props}
       >
         {children}
